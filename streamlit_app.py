ed6bd63cd24639cba566d6304c7f5a5150c82940
ruydoitgame <ruydoitgame@gmail.com>
2026-03-10 10:24:40 +0700

    Read and display Google Sheets data in Streamlit
    Add functionality to read and display Google Sheets data as a CSV.

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,17 @@ import pandas as pd
 st.title("Trang web hiển thị Google Sheets của tôi 🚀")
 
 
+# Chuyển link Google Sheet sang dạng xuất file CSV
+sheet_url = "https://docs.google.com/spreadsheets/d/1_l2HMO65LAPR9MDTpCXCN7oLfpvyVBziRoJ282Ew2Jc/edit?gid="
+csv_url = sheet_url.replace("/edit#gid=", "/export?format=csv&gid=")
+
+# Đọc dữ liệu
+df = pd.read_csv(csv_url)
+
+# Hiển thị lên web
 st.write("Dữ liệu mới nhất:")
+st.dataframe(df)
+
+st.bar_chart(df.iloc[:, 1]) # Thử vẽ biểu đồ cột cho cột thứ 2
 
 
